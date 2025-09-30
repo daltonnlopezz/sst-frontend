ad2fa580c57da1d08c819b6027b09e1ddef335d1
Dalton Lopez <daltonnlopezz@gmail.com>
2025-09-29 22:05:37 -0700

    Initial commit

--- __init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Frontend package for SST.2.0
-Web interface for the SAM Opportunity Search Service
-"""
-
-
-
--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Production startup script for Render
-
-# Install dependencies
-pip install -r requirements.txt
-
-# Start the application with Gunicorn
-gunicorn --bind 0.0.0.0:$PORT app:app
